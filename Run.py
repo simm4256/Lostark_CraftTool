de6f60d2646d33dcb7bd419e85096c3d70769de2
simm4256 <simm4256@naver.com>
2021-10-26 18:04:41 +0900

    update 3.1.0.0

--- Run.py
+++ Run.py
@@ -4,6 +4,8 @@ import time
 import openpyxl
 import re
 import pyperclip
+import sys
+import os
 
 def Search(s):
     browser.execute_script('selectCategory(0,0, true);')
@@ -51,6 +53,12 @@ def Login():
     time.sleep(2)
     ChangeWindow(0)
 
+def Message():
+    global progress
+    progress += 1
+    msg = "\r진행률 : %d%%"%(progress)
+    print(msg, end='')
+
 options = webdriver.ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
 browser = webdriver.Chrome('chromedriver.exe', options=options)
@@ -59,39 +67,40 @@ browser.get('https://lostark.game.onstove.com/Market')
 wb = openpyxl.load_workbook('base.xlsm', read_only=False, keep_vba=True)
 ws = wb['거래소 최저가']
 
-try:
-    login_type = wb['검색']['I5'].internal_value
-    uid = wb['검색']['I6'].internal_value
-    upw = wb['검색']['I7'].internal_value
-except:
+os.system('cls')
+print('\n','-'*43,'\n Lostark Craft Tool ver3.1.0.0 by simm4256\n','-'*43,'\n')
+
+login_type = wb['검색']['I5'].internal_value
+uid = wb['검색']['I6'].internal_value
+upw = wb['검색']['I7'].internal_value
+
+if login_type==None or uid==None or upw==None:
+    browser.quit()
     print("로그인 정보 불러오기에 실패했습니다.")
     print("base.xlsm 파일에 로그인 정보를 잘 입력했는지 확인해주세요.")
     print("만약 Readme.txt를 읽지 않으셨다면 반드시 읽어주세요.")
-
+    print("이 창은 10초 후 닫힙니다.")
+    time.sleep(10)
+    sys.exit()
 
 Login()
+progress=-1
+Message()
 
-print('\n최저가 데이터 받아오는 중...')
-
-
-
-
-
-
-browser.find_element_by_css_selector('.main-category > li:nth-child(8) > a:nth-child(1)').click()
-browser.find_element_by_css_selector('.is-active > ul:nth-child(2) > li:nth-child(1) > a:nth-child(1)').click()
-time.sleep(1.5)
-browser.find_element_by_css_selector('#itemList > thead:nth-child(3) > tr:nth-child(1) > th:nth-child(1) > a:nth-child(1)').click()
+browser.find_element_by_css_selector('.main-category > li:nth-child(8) > a:nth-child(1)').click(); Message()
+browser.find_element_by_css_selector('.is-active > ul:nth-child(2) > li:nth-child(1) > a:nth-child(1)').click(); Message()
+time.sleep(1.5); Message()
+browser.find_element_by_css_selector('#itemList > thead:nth-child(3) > tr:nth-child(1) > th:nth-child(1) > a:nth-child(1)').click(); Message()
 
 cnt=0
 j=1
 for k in range(1,5) :
     if k>1 :
         browser.execute_script('paging.page({});'.format(k))
-    time.sleep(0.7)
-    prices = browser.find_elements_by_class_name("price")
-    names = browser.find_elements_by_class_name('name')
-    cnt_name = 1
+    time.sleep(0.7); Message(); Message(); Message(); Message(); Message(); Message()
+    prices = browser.find_elements_by_class_name("price"); Message()
+    names = browser.find_elements_by_class_name('name'); Message()
+    cnt_name = 1; Message()
     for i in prices:
         cnt+=1
         if cnt==3 :
@@ -108,7 +117,7 @@ for k in range(1,5) :
 
 browser.find_element_by_css_selector('.main-category > li:nth-child(6) > a:nth-child(1)').click()
 browser.find_element_by_css_selector('.is-active > ul:nth-child(2) > li:nth-child(1) > a:nth-child(1)').click()
-time.sleep(1)
+time.sleep(1); Message()
 browser.find_element_by_css_selector('#itemList > thead:nth-child(3) > tr:nth-child(1) > th:nth-child(1) > a:nth-child(1)').click()
 
 cnt=0
@@ -116,7 +125,7 @@ j=1
 for k in range(1,7) :
     if k>1 :
         browser.execute_script('paging.page({});'.format(k))
-    time.sleep(0.7)
+    time.sleep(0.7); Message(); Message(); Message(); Message(); Message(); Message()
     prices = browser.find_elements_by_class_name("price")
     names = browser.find_elements_by_class_name('name')
     cnt_name = 1
@@ -135,21 +144,21 @@ Search('하 융')
 browser.find_element_by_css_selector('#itemList > thead:nth-child(3) > tr:nth-child(1) > th:nth-child(1) > a:nth-child(1)').click()
 time.sleep(0.5)
 prices = browser.find_elements_by_class_name("price")
-ws['G61'] = int(prices[2].text.replace(',',""))
-ws['G62'] = int(prices[5].text.replace(',',""))
-ws['G63'] = int(prices[8].text.replace(',',""))
+ws['G61'] = int(prices[2].text.replace(',',"")); Message()
+ws['G62'] = int(prices[5].text.replace(',',"")); Message()
+ws['G63'] = int(prices[8].text.replace(',',"")); Message()
 
 Search('가루')
 prices = browser.find_elements_by_class_name("price")
-ws['G67'] = int(prices[2].text.replace(',',""))
+ws['G67'] = int(prices[2].text.replace(',',"")); Message()
 
 Search('정식')
 browser.find_element_by_css_selector("#lostark-wrapper > div > main > div > div.deal > div.deal-contents > form > fieldset > div > div.detail > div.grade > div > div.lui-select__title").click()
-time.sleep(0.5)
+time.sleep(0.5); Message()
 browser.find_element_by_css_selector("#lostark-wrapper > div > main > div > div.deal > div.deal-contents > form > fieldset > div > div.detail > div.grade > div > div.lui-select__option > label:nth-child(4)").click()
-time.sleep(0.5)
+time.sleep(0.5); Message()
 browser.find_element_by_css_selector("#lostark-wrapper > div > main > div > div.deal > div.deal-contents > form > fieldset > div > div.bt > button.button.button--deal-submit").click()
-time.sleep(0.5)
+time.sleep(0.5); Message()
 prices = browser.find_elements_by_class_name("price")
 names = browser.find_elements_by_class_name('name')
 cnt_name = 0
@@ -162,7 +171,7 @@ for i in prices:
         cnt_name+=1
         try : 
             tmp = browser.find_element_by_css_selector('#tbodyItemList > tr:nth-child({}) > td:nth-child(1) > div:nth-child(1) > span:nth-child(3)'.format(cnt_name)).text
-            ws['F{}'.format(j)]=names[cnt_name].text
+            ws['F{}'.format(j)]=names[cnt_name].text; Message(); Message()
             p=int(i.text.replace(',',""))
             ws['G{}'.format(j)] = p
             j+=1        
@@ -170,8 +179,9 @@ for i in prices:
 
 
 t = time.strftime('%m%d_%H%M%S',time.localtime(time.time()))
-wb.save('{}.xlsm'.format(t))
+wb.save('{}.xlsm'.format(t)); Message()
+progress=99; Message()
 
-print('최저가 데이터 동기화 완료. {}.xlsm 파일에서 확인하세요.\n이 창은 5초 후 자동으로 꺼집니다.'.format(t))
+print('\n\n최저가 데이터 동기화 완료. \n{}.xlsm 파일에서 확인하세요.\n이 창은 5초 후 자동으로 꺼집니다.'.format(t))
 time.sleep(5)
 browser.quit()
