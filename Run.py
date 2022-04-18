f752ec504825fc4773dd16913b88f117a1dd7da1
simm4256 <simm4256@naver.com>
2022-04-19 01:37:43 +0900

    update 3.3.0.0

--- Run.py
+++ Run.py
@@ -1,5 +1,10 @@
+from asyncio.windows_events import NULL
+from concurrent.futures import process
+from pickle import NONE, TRUE
+from tkinter import E
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 import time
 import openpyxl
 import re
@@ -7,22 +12,22 @@ import pyperclip
 import sys
 import os
 
-def Search(s):
-    browser.execute_script('selectCategory(0,0, true);')
-    Search = browser.find_element_by_id('txtItemName')
+def search(s):
+    excuteScript('selectCategory(0,0, true);')
+    Search = getElement('ID', 'txtItemName')
     Search.clear()
     Search.send_keys(s)
     Search.send_keys('\n')
     time.sleep(1.5)
 
-def ChangeWindow(n):
+def changeWindow(n):
     browser.switch_to.window(browser.window_handles[n])
 
-def Login():
+def login():
     if login_type != "stove":
-        browser.find_element_by_id("{}_login".format(login_type)).click()
+        getElement('ID', "{}_login".format(login_type)).click()
         time.sleep(1)
-        ChangeWindow(1)
+        changeWindow(1)
 
     sid = {"stove"  : "user_id", 
            "naver"  : "id",
@@ -37,27 +42,59 @@ def Login():
               "facebook" : "#loginbutton",
               "twitter" : "#allow"}
 
-    bid = browser.find_element_by_id(sid[login_type])
+    bid = getElement('ID', sid[login_type])
     bid.click()
     pyperclip.copy(uid)
     bid.send_keys(Keys.CONTROL, 'v')
-    time.sleep(1)
+    time.sleep(0.5)
 
-    bpw = browser.find_element_by_id(spw[login_type])
+    bpw = getElement('ID', spw[login_type])
     bpw.click()
     pyperclip.copy(upw)
     bpw.send_keys(Keys.CONTROL, 'v')
-    time.sleep(1)
+    time.sleep(0.5)
     
-    browser.find_element_by_css_selector(sclick[login_type]).click()
-    time.sleep(2)
-    ChangeWindow(0)
+    try:
+        getElement('CSS_SELECTOR', sclick[login_type]).click()
+        time.sleep(2)
+        changeWindow(0)
+    except:
+        NONE
 
-def Message():
+def showMessage():
     global progress
     progress += 1
     msg = "\r진행률 : %d%%"%(progress)
     print(msg, end='')
+
+def getElement(by, val, isMany=False, _range=20, ignore=False):
+    global bys, browser, err
+    for i in range(_range):
+        try:
+            if isMany:
+                return browser.find_elements(by=bys[by],value=val)
+            else:                
+                return browser.find_element(by=bys[by],value=val)
+        except:
+            if ignore:
+                return NULL
+            time.sleep(0.5)
+    print("ERROR : ({}) when getElement {val}".format(err))
+    time.sleep(5)
+    sys.exit()
+
+def excuteScript(val, _range=20):
+    global browser, err
+    for i in range(_range):
+        try:
+            browser.execute_script(val)
+            return
+        except:
+            time.sleep(0.5)
+    print("ERROR : ({}) when getScript {val}".format(err))
+    time.sleep(5)
+    sys.exit()
+    
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option('excludeSwitches', ['enable-logging'])
@@ -67,12 +104,23 @@ browser.get('https://lostark.game.onstove.com/Market')
 wb = openpyxl.load_workbook('base.xlsm', read_only=False, keep_vba=True)
 ws = wb['거래소 최저가']
 
+bys = {
+    'CLASS_NAME' : By.CLASS_NAME,
+    'CSS_SELECTOR' : By.CSS_SELECTOR,
+    'ID' : By.ID,
+}
+
 os.system('cls')
-print('\n','-'*43,'\n Lostark Craft Tool ver3.2.0.0 by simm4256\n','-'*43,'\n')
+print('\n','-'*43,'\n Lostark Craft Tool ver3.3.0.0 by 리퍼가신은신발은슬리퍼\n','-'*43,'\n')
 
 login_type = wb['검색']['I5'].internal_value
 uid = wb['검색']['I6'].internal_value
 upw = wb['검색']['I7'].internal_value
+
+# -------------------- Login --------------------
+
+print("로봇 방지 등의 이유로 자동 로그인이 안됐다면 2분 내에 수동으로 로그인해주세요.")
+print("로그인하면 자동으로 최저가를 탐색합니다.\n")
 
 if login_type==None or uid==None or upw==None:
     browser.quit()
@@ -84,8 +132,9 @@ if login_type==None or uid==None or upw==None:
     sys.exit()
 
 try:
-    Login()
-except:
+    login()
+except Exception as e:
+    print(e)
     print("로그인 과정에서 오류가 생겼습니다.")
     print("STOVE 계정이 아니라면 STOVE 계정으로 시도해보시기 바랍니다.")
     print("이 오류가 반복될 경우 제작자에게 문의해주세요.")
@@ -93,122 +142,127 @@ except:
     time.sleep(10)
     sys.exit()
 
-isError = True
-for i in range(5):
-    try:
-        progress=-1
-        err = 1
+# -------------------- Logic --------------------
 
-        Message()
-        browser.find_element_by_css_selector('.main-category > li:nth-child(8) > a:nth-child(1)').click(); Message()
-        browser.find_element_by_css_selector('.is-active > ul:nth-child(2) > li:nth-child(1) > a:nth-child(1)').click(); Message(); err+=1
-        time.sleep(1.5); Message()
-        browser.find_element_by_css_selector('#itemList > thead:nth-child(3) > tr:nth-child(1) > th:nth-child(1) > a:nth-child(1)').click(); Message(); err+=1
+try:
+    progress=-1
+    err = 1
+    elem = NULL
+    elems = NULL
 
-        cnt=0
-        j=1
-        for k in range(1,5) :
-            if k>1 :
-                browser.execute_script('paging.page({});'.format(k))
-            err+=1
-            time.sleep(0.7); Message(); Message(); Message(); Message(); Message(); Message()
-            prices = browser.find_elements_by_class_name("price"); Message()
-            names = browser.find_elements_by_class_name('name'); Message()
-            cnt_name = 1; Message()
-            for i in prices:
-                cnt+=1
-                if cnt==3 :
-                    cnt=0
-                    j+=1
-                    ws['A{}'.format(j)]=names[cnt_name].text
-                    try : 
-                        ws['D{}'.format(j)] = re.sub(r'[^0-9]', '', (browser.find_element_by_css_selector('#tbodyItemList > tr:nth-child({}) > td:nth-child(1) > div:nth-child(1) > span:nth-child(3)'.format(cnt_name)).text))
-                    except : None
-                    cnt_name+=1
-                    p=int(i.text.replace(',',""))
-                    ws['B{}'.format(j)] = p
-            j+=1; err+=1
-        browser.find_element_by_css_selector('.main-category > li:nth-child(6) > a:nth-child(1)').click()
-        browser.find_element_by_css_selector('.is-active > ul:nth-child(2) > li:nth-child(1) > a:nth-child(1)').click(); err+=1
-        time.sleep(1)
-        browser.find_element_by_css_selector('#itemList > thead:nth-child(3) > tr:nth-child(1) > th:nth-child(1) > a:nth-child(1)').click(); err+=1
+    elem = getElement('CSS_SELECTOR', '.main-category > li:nth-child(8) > a:nth-child(1)', _range=240)
 
-        cnt=0
-        j=1
-        for k in range(1,7) :
-            if k>1 :
-                browser.execute_script('paging.page({});'.format(k))
-            err+=1
-            time.sleep(0.7); Message(); Message(); Message(); Message(); Message(); Message()
-            prices = browser.find_elements_by_class_name("price")
-            names = browser.find_elements_by_class_name('name')
-            cnt_name = 1
-            for i in prices:
-                cnt+=1
-                if cnt==3 :
-                    cnt=0
-                    j+=1
-                    ws['F{}'.format(j)]=names[cnt_name].text
-                    cnt_name+=1
-                    p=int(i.text.replace(',',""))
-                    ws['G{}'.format(j)] = p
-            j+=1; err+=1
+    err+=1
+    #progress:0
+    showMessage()
+    getElement('CSS_SELECTOR', '.main-category > li:nth-child(8) > a:nth-child(1)').click(); showMessage()
+    getElement('CSS_SELECTOR', '.is-active > ul:nth-child(2) > li:nth-child(1) > a:nth-child(1)').click(); showMessage(); err+=1
+    time.sleep(1.5); showMessage()
+    getElement('CSS_SELECTOR', '#itemList > thead:nth-child(3) > tr:nth-child(1) > th:nth-child(1) > a:nth-child(1)').click(); showMessage(); err+=1
 
-        Search('하 융'); Message(); Message(); Message(); err+=1
-        browser.find_element_by_css_selector('#itemList > thead:nth-child(3) > tr:nth-child(1) > th:nth-child(1) > a:nth-child(1)').click(); err+=1
-        time.sleep(0.5)
-        prices = browser.find_elements_by_class_name("price"); err+=1
-        ws['G61'] = int(prices[2].text.replace(',',"")); Message()
-        ws['G62'] = int(prices[5].text.replace(',',"")); Message()
-        ws['G63'] = int(prices[8].text.replace(',',"")); Message()
-
-        Search('가루'); Message(); Message(); Message(); Message(); err+=1
-        prices = browser.find_elements_by_class_name("price"); err+=1
-        ws['G67'] = int(prices[2].text.replace(',',""))
-
-        Search('정식'); err+=1
-        browser.find_element_by_css_selector("#lostark-wrapper > div > main > div > div.deal > div.deal-contents > form > fieldset > div > div.detail > div.grade > div > div.lui-select__title").click()
-        time.sleep(0.5); Message(); Message(); err+=1
-        browser.find_element_by_css_selector("#lostark-wrapper > div > main > div > div.deal > div.deal-contents > form > fieldset > div > div.detail > div.grade > div > div.lui-select__option > label:nth-child(4)").click()
-        time.sleep(0.5); Message(); Message(); err+=1
-        browser.find_element_by_css_selector("#lostark-wrapper > div > main > div > div.deal > div.deal-contents > form > fieldset > div > div.bt > button.button.button--deal-submit").click()
-        time.sleep(0.5); Message(); Message(); err+=1
-        prices = browser.find_elements_by_class_name("price")
-        names = browser.find_elements_by_class_name('name'); err+=1
-        cnt_name = 0
-        cnt=0
-        j=69
+    cnt=0
+    j=1
+    #progress:4
+    for k in range(1,5) :
+        if k>1 :
+            excuteScript('paging.page({});'.format(k))
+        err+=1
+        time.sleep(0.7); showMessage(); showMessage(); showMessage(); showMessage(); showMessage(); showMessage()
+        prices = getElement('CLASS_NAME', "price", True); showMessage()
+        names = getElement('CLASS_NAME', 'name', True); showMessage()
+        cnt_name = 1; showMessage()
         for i in prices:
             cnt+=1
             if cnt==3 :
                 cnt=0
-                cnt_name+=1
+                j+=1
+                ws['A{}'.format(j)]=names[cnt_name].text
                 try : 
-                    tmp = browser.find_element_by_css_selector('#tbodyItemList > tr:nth-child({}) > td:nth-child(1) > div:nth-child(1) > span:nth-child(3)'.format(cnt_name)).text; err+=1
-                    ws['F{}'.format(j)]=names[cnt_name].text; Message(); Message()
-                    p=int(i.text.replace(',',""))
-                    ws['G{}'.format(j)] = p
-                    j+=1        
+                    ws['D{}'.format(j)] = re.sub(r'[^0-9]', '', (getElement('CSS_SELECTOR', '#tbodyItemList > tr:nth-child({}) > td:nth-child(1) > div:nth-child(1) > span:nth-child(3)'.format(cnt_name), ignore=True).text))
                 except : None
+                cnt_name+=1
+                p=int(i.text.replace(',',""))
+                ws['B{}'.format(j)] = p
+        j+=1; err+=1
+    getElement('CSS_SELECTOR', '.main-category > li:nth-child(6) > a:nth-child(1)').click()
+    getElement('CSS_SELECTOR', '.is-active > ul:nth-child(2) > li:nth-child(1) > a:nth-child(1)').click(); err+=1
+    time.sleep(1)
+    getElement('CSS_SELECTOR', '#itemList > thead:nth-child(3) > tr:nth-child(1) > th:nth-child(1) > a:nth-child(1)').click(); err+=1
 
-        err = 9999
-        t = time.strftime('%m%d_%H%M%S',time.localtime(time.time()))
-        wb.save('{}.xlsm'.format(t)); Message()
-        progress=99; Message()
+    cnt=0
+    j=1
+    #progress:40
+    for k in range(1,7) :
+        if k>1 :
+            excuteScript('paging.page({});'.format(k))
+        err+=1
+        time.sleep(0.7); showMessage(); showMessage(); showMessage(); showMessage(); showMessage(); showMessage()
+        prices = getElement('CLASS_NAME', "price", True)
+        names = getElement('CLASS_NAME', 'name', True)
+        cnt_name = 1
+        for i in prices:
+            cnt+=1
+            if cnt==3 :
+                cnt=0
+                j+=1
+                ws['F{}'.format(j)]=names[cnt_name].text
+                cnt_name+=1
+                p=int(i.text.replace(',',""))
+                ws['G{}'.format(j)] = p
+        j+=1; err+=1
 
-        print('\n\n최저가 데이터 동기화 완료. \n{}.xlsm 파일에서 확인하세요.\n이 창은 5초 후 자동으로 꺼집니다.'.format(t))
-        time.sleep(5)
-        browser.quit()
-        isError = False
-        break
-    except:
-        print("\n최저가 탐색 중 오류 발생! ERROR : {}".format(err))
-        print("동일한 에러가 반복될 경우 제작자에게 문의해주세요.")
-        print("5초 후 처음부터 다시 탐색합니다.")
-        time.sleep(5)
+    #progress:76
+    search('하 융'); showMessage(); showMessage(); showMessage(); err+=1
+    getElement('CSS_SELECTOR', '#itemList > thead:nth-child(3) > tr:nth-child(1) > th:nth-child(1) > a:nth-child(1)').click(); err+=1
+    time.sleep(0.5)
+    prices = getElement('CLASS_NAME', "price", True); err+=1
+    ws['G61'] = int(prices[2].text.replace(',',"")); showMessage()
+    ws['G62'] = int(prices[5].text.replace(',',"")); showMessage()
+    ws['G63'] = int(prices[8].text.replace(',',"")); showMessage()
 
-if isError:
-    print("\n최저가 탐색 중 오류 발생! ERROR : {}".format(err))
-    print("동일한 에러가 반복될 경우 제작자에게 문의해주세요.")
-    print("에러가 5회 발생하여 5초 후 프로그램이 종료됩니다.")
+    #progress:83
+    search('가루'); showMessage(); showMessage(); showMessage(); showMessage(); err+=1
+    prices = getElement('CLASS_NAME', "price", True); err+=1
+    ws['G67'] = int(prices[2].text.replace(',',""))
+    
+    #progress:86
+    search('정식'); err+=1
+    getElement('CSS_SELECTOR', "#lostark-wrapper > div > main > div > div.deal > div.deal-contents > form > fieldset > div > div.detail > div.grade > div > div.lui-select__title").click()
+    time.sleep(0.5); showMessage(); showMessage(); err+=1
+    getElement('CSS_SELECTOR', "#lostark-wrapper > div > main > div > div.deal > div.deal-contents > form > fieldset > div > div.detail > div.grade > div > div.lui-select__option > label:nth-child(4)").click()
+    time.sleep(0.5); showMessage(); showMessage(); err+=1
+    getElement('CSS_SELECTOR', "#lostark-wrapper > div > main > div > div.deal > div.deal-contents > form > fieldset > div > div.bt > button.button.button--deal-submit").click()
+    time.sleep(0.5); showMessage(); showMessage(); err+=1
+    prices = getElement('CLASS_NAME', "price", True)
+    names = getElement('CLASS_NAME', 'name', True); err+=1
+    cnt_name = 0
+    cnt=0
+    j=69
+    for i in prices:
+        cnt+=1
+        if cnt==3 :
+            cnt=0
+            cnt_name+=1
+            try : 
+                err+=1
+                tmp = getElement('CSS_SELECTOR', '#tbodyItemList > tr:nth-child({}) > td:nth-child(1) > div:nth-child(1) > span:nth-child(3)'.format(cnt_name), ignore=True).text; 
+                ws['F{}'.format(j)]=names[cnt_name].text; showMessage(); showMessage()
+                p=int(i.text.replace(',',""))
+                ws['G{}'.format(j)] = p
+                j+=1        
+            except : None
+
+    err = 9999
+    t = time.strftime('%m%d_%H%M%S',time.localtime(time.time()))
+    wb.save('{}.xlsm'.format(t)); showMessage()
+    progress=99; showMessage()
+
+    print('\n\n최저가 데이터 동기화 완료. \n{}.xlsm 파일에서 확인하세요.\n이 창은 5초 후 자동으로 꺼집니다.'.format(t))
     time.sleep(5)
+    browser.quit()
+except Exception as e:
+    print("ERROR Progress:{}".format(progress))
+    print("동일한 에러가 반복될 경우 제작자에게 문의해주세요.")
+    print("5초 후 종료됩니다.")
+    time.sleep(5)
+    
